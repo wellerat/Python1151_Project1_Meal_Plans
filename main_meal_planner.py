c52d8327fdadca793e97f12737812979acd8b93c
Ann Cooper <wellerat@yahoo.com>
2026-09-23 21:41:24 -0400

    Added the functinality for the user to add meals to te list

--- main_meal_planner.py
+++ main_meal_planner.py
@@ -14,22 +14,33 @@ Starter code:
 Date:
     Sept. 21, 2026
 """
-from meals import meals
+from meals import meals, add_additional_meals
 import random
 
 days = ["Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 def main():
 
     print("\n----     Meal Planner     ----")
-    num_of_days = int(input("\nMeal planning for how many days (1-7)?"))
-    start_day = input("\nStarting on what day? (ex. Monday)")
+    add_meals = input('\nWould you like to add meals to the current options? (Y/N)   ')
+
+    while (add_meals != 'Y') and (add_meals != 'N'):
+        print('\nInvalid response please pick "N" or "Y"')
+        add_meals = input('\nWould you like to add meals to the current options? (Y/N)   ')
+
+    if add_meals == 'Y':
+        add_additional_meals()
+
+    num_of_days = int(input("\nMeal planning for how many days (1-7)?   "))
+    start_day = input("\nStarting on what day? (ex. Monday)   ")
+
+    
 
     start_day_index = days.index(start_day)
 
     plan_days = []
 
     for i in range(num_of_days):
-        day = days[(start_day_index + i)%7]4
+        day = days[(start_day_index + i)%7]
         plan_days.append(day)
 
     for i in range(num_of_days):
--- meals.py
+++ meals.py
@@ -35,3 +35,35 @@ meals = {
     ]
 
 }
+
+def add_additional_meals():
+    """Add additional meals to breakfast, lunch or dinner until user quits"""
+
+    meal_option = '';
+
+    while meal_option != "Q":
+        print("\nWhat meal category do you want to add? ")
+        print("\nBreakfast (B), Lunch (L), Dinner (D), or Quit (Q)")
+        meal_option = input("\nEnter your option: B, L, D, or Q:   ").upper()
+
+        while meal_option not in ('B','L','D','Q'):
+            print("Please enter B, L, D or Q")
+            meal_option = input("\nEnter your option: B, L, D, or Q:   ").upper()
+
+        if meal_option == "Q":
+            break
+
+        meal_description = input("\nEnter additional meal:")
+
+        if meal_option == 'B':   
+            meals["breakfast"].append(meal_description)
+        elif meal_option == 'L':
+            meals["lunch"].append(meal_description)
+        elif meal_option == 'D':
+            meals["dinner"].append(meal_description)
+
+        print(f"\n{meal_description} has been added.")
+
+    print("\nAdding new meals complete.")   
+
+
