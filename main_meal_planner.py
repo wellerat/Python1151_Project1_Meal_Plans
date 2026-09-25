975c57f302f611f5025704bbe39bcdbd794c9571
Ann Cooper <wellerat@yahoo.com>
2026-09-25 15:55:25 -0400

    Added two function to validate the  number of days value and the start day value

--- main_meal_planner.py
+++ main_meal_planner.py
@@ -19,7 +19,39 @@ from print_meals import print_meals
 import random
 
 days = ["Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+def get_days():
+    """Ask the user for number of days (1-7) and validate input"""
+    while True:
+        value = input("\nMeal planning for how many days (1-7)?   ")
+
+        try:
+            num_days = int(value)
+        except ValueError:
+            print("Invalid input.  Please enter a whole number.")
+            continue
+
+        if 1 <= num_days <= 7:
+            return num_days
+        else:
+            print("Value out of range. Please enter a number between 1 and 7.")
+
+
+def get_start_day():
+    """Ask the user for a valid start day and check input"""
+    while True:
+        value = input(f"\nStarting on what day? (ex. Monday):  ")     
+
+        value = value.capitalize()
+
+        if value in days:
+            return value
+        else:
+            print(f"Invalid entry.  Please choose one of {days}")
+
+
 def main():
+    """Main program flow for the program Meal Planner"""
 
     print("\n\n               ----     Meal Planner    ----")
     print('\n\nThis program will create a meal plan for up to a week.')
@@ -33,8 +65,9 @@ def main():
     if add_meals == 'Y':
         add_additional_meals()
 
-    num_of_days = int(input("\nMeal planning for how many days (1-7)?   "))
-    start_day = input("\nStarting on what day? (ex. Monday)   ")
+    num_of_days = get_days()
+
+    start_day = get_start_day()
 
     print_meals(num_of_days,start_day,days)
     
